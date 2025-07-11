b2ce147b4c84a8c3aeaa980edae5721fd581e04a
Ardhi Putra Pratama H <me@ardhipoetra.web.id>
2025-07-11 11:02:02 +0200

    Remove sgx*.conf duplicates since they already in  dir

--- build-pravega/prepare_pravega.sh
+++ build-pravega/prepare_pravega.sh
@@ -39,14 +39,11 @@ echo "---- Applying Pravega-compose Changes ----"
 cp pravega-changes/docker-compose/docker-compose-nfs.yml pravega/docker/compose/docker-compose-nfs.yml
 
 # add soft link to docker-compose.yml for easier debug
-ln -s pravega/docker/compose/docker-compose-nfs.yml docker-compose-nfs.yml
+ln -s pravega/docker/compose/docker-compose-nfs.yml $(dirname "$0")/../docker-compose-nfs.yml
 
 cat > $(dirname "$0")/../.env <<EOF
 # PRAVEGA_BK_IMAGE=pravega_s:bk
 PRAVEGA_IMAGE=pravega_s:${PRAVEGA_VERSION}.0
 EOF
-
-# copy SCONE configuration to compose directory
-cp pravega-changes/docker-compose/sgx-musl-*.conf pravega/docker/compose/
 
 echo "Done"
